98c211bd71cd53e283d70e09c21ed8c9cbab6c2b
zhangyanbing <zhangyanbing97@zto.com>
2021-11-29 15:28:34 +0800

    添加工具类

---
+++ dabaicai-common/src/main/java/com/dabaicai/framework/common/utils/ClassUtils.java
@@ -0,0 +1,10 @@
+package com.dabaicai.framework.common.utils;
+
+/**
+ * 类工具类
+ *
+ * @author: zhangyanbing97
+ * Date: 2021/11/29 15:26
+ */
+public class ClassUtils {
+}
---
+++ dabaicai-common/src/main/java/com/dabaicai/framework/common/utils/ListUtils.java
@@ -0,0 +1,10 @@
+package com.dabaicai.framework.common.utils;
+
+/**
+ * list工具类
+ *
+ * @author: zhangyanbing97
+ * Date: 2021/11/29 15:26
+ */
+public class ListUtils {
+}
